4ab9f5b76aa1a498c230eca6f6583878e5d6d741
Rexarrior <Rexarrior@yandex.ru>
2018-12-14 00:32:26 +0300

    s

--- DATABASE/проект/create.sql
+++ DATABASE/проект/create.sql
@@ -18,146 +18,160 @@ CREATE SCHEMA IF NOT EXISTS `cyberlife` DEFAULT CHARACTER SET latin1 ;
 USE `cyberlife` ;
 
 -- -----------------------------------------------------
--- Table `cyberlife`.`words`
+-- Table `cyberlife`.`worlds`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cyberlife`.`words` (
-  `word_id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `cyberlife`.`worlds` (
+  `world_id` INT(11) NOT NULL AUTO_INCREMENT,
   `map` MULTIPOINT NOT NULL,
   `name` VARCHAR(45) NOT NULL,
-  `description` VARCHAR(45) NULL,
-  PRIMARY KEY (`word_id`),
-  UNIQUE INDEX `word_id_UNIQUE` (`word_id` ASC) ,
+  `description` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`world_id`),
+  UNIQUE INDEX `world_id_UNIQUE` (`world_id` ASC) ,
   UNIQUE INDEX `name_UNIQUE` (`name` ASC) )
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
--- Table `cyberlife`.`word_lifeform_sets`
+-- Table `cyberlife`.`world_lifeform_sets`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cyberlife`.`word_lifeform_sets` (
-  `lifeform_set_id` INT NOT NULL AUTO_INCREMENT,
-  `description` VARCHAR(45) NULL,
+CREATE TABLE IF NOT EXISTS `cyberlife`.`world_lifeform_sets` (
+  `lifeform_set_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `description` VARCHAR(45) NULL DEFAULT NULL,
   `name` VARCHAR(45) NOT NULL,
-  `word_id_fk` INT NOT NULL,
-  PRIMARY KEY (`lifeform_set_id`, `word_id_fk`),
+  `world_id_fk` INT(11) NOT NULL,
+  PRIMARY KEY (`lifeform_set_id`, `world_id_fk`),
   UNIQUE INDEX `lifeform_set_id_UNIQUE` (`lifeform_set_id` ASC) ,
-  INDEX `fk_word_lifeform_sets_words1_idx` (`word_id_fk` ASC) ,
-  CONSTRAINT `fk_word_lifeform_sets_words1`
-    FOREIGN KEY (`word_id_fk`)
-    REFERENCES `cyberlife`.`words` (`word_id`)
+  INDEX `fk_world_lifeform_sets_worlds1_idx` (`world_id_fk` ASC) ,
+  CONSTRAINT `fk_world_lifeform_sets_worlds1`
+    FOREIGN KEY (`world_id_fk`)
+    REFERENCES `cyberlife`.`worlds` (`world_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
 -- Table `cyberlife`.`lifeforms`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `cyberlife`.`lifeforms` (
-  `lifeform_id` INT NOT NULL AUTO_INCREMENT,
+  `lifeform_id` INT(11) NOT NULL AUTO_INCREMENT,
   `point` POINT NOT NULL,
-  `genom` VARCHAR(45) NOT NULL,
-  `word_id_fk` INT NOT NULL,
-  `lifeform_set_id_fk` INT NOT NULL,
-  PRIMARY KEY (`lifeform_id`, `word_id_fk`, `lifeform_set_id_fk`),
+  `genom` VARCHAR(1000) NOT NULL,
+  `world_id_fk` INT(11) NOT NULL,
+  `lifeform_set_id_fk` INT(11) NOT NULL,
+  PRIMARY KEY (`lifeform_id`, `world_id_fk`, `lifeform_set_id_fk`),
   UNIQUE INDEX `lifeform_id_UNIQUE` (`lifeform_id` ASC) ,
-  INDEX `fk_lifeforms_words1_idx` (`word_id_fk` ASC) ,
-  INDEX `fk_lifeforms_word_lifeform_sets1_idx` (`lifeform_set_id_fk` ASC) ,
-  CONSTRAINT `fk_lifeforms_words1`
-    FOREIGN KEY (`word_id_fk`)
-    REFERENCES `cyberlife`.`words` (`word_id`)
+  INDEX `fk_lifeforms_worlds1_idx` (`world_id_fk` ASC) ,
+  INDEX `fk_lifeforms_world_lifeform_sets1_idx` (`lifeform_set_id_fk` ASC) ,
+  CONSTRAINT `fk_lifeforms_worlds1`
+    FOREIGN KEY (`world_id_fk`)
+    REFERENCES `cyberlife`.`worlds` (`world_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_lifeforms_word_lifeform_sets1`
+  CONSTRAINT `fk_lifeforms_world_lifeform_sets1`
     FOREIGN KEY (`lifeform_set_id_fk`)
-    REFERENCES `cyberlife`.`word_lifeform_sets` (`lifeform_set_id`)
+    REFERENCES `cyberlife`.`world_lifeform_sets` (`lifeform_set_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 21
+DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
 -- Table `cyberlife`.`states`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `cyberlife`.`states` (
-  `state_id` INT NOT NULL AUTO_INCREMENT,
+  `state_id` INT(11) NOT NULL AUTO_INCREMENT,
   `state_type` VARCHAR(45) NULL DEFAULT 'double',
-  `description` MULTILINESTRING NULL,
+  `description` MULTILINESTRING NULL DEFAULT NULL,
   `name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`state_id`),
   UNIQUE INDEX `state_id_UNIQUE` (`state_id` ASC) ,
   UNIQUE INDEX `name_UNIQUE` (`name` ASC) )
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `cyberlife`.`phenomens`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cyberlife`.`phenomens` (
-  `phenomen_id` INT NOT NULL AUTO_INCREMENT,
-  `effect` DOUBLE NOT NULL,
-  `description` VARCHAR(45) NULL,
-  `state_id_fk` INT NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`phenomen_id`, `state_id_fk`),
-  UNIQUE INDEX `phenomen_id_UNIQUE` (`phenomen_id` ASC) ,
-  INDEX `fk_phenomens_states1_idx` (`state_id_fk` ASC) ,
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC) ,
-  CONSTRAINT `fk_phenomens_states1`
-    FOREIGN KEY (`state_id_fk`)
-    REFERENCES `cyberlife`.`states` (`state_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 5
+DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
 -- Table `cyberlife`.`lifeform_states`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `cyberlife`.`lifeform_states` (
-  `value` DOUBLE NOT NULL DEFAULT 0,
-  `state_id_fk` INT NOT NULL,
-  `lifeform_id_fk` INT NOT NULL,
-  PRIMARY KEY (`state_id_fk`, `lifeform_id_fk`),
-  INDEX `fk_lifeform_states_lifeforms1_idx` (`lifeform_id_fk` ASC) ,
-  CONSTRAINT `fk_lifeform_states_states`
-    FOREIGN KEY (`state_id_fk`)
-    REFERENCES `cyberlife`.`states` (`state_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  `value` DOUBLE NOT NULL DEFAULT '0',
+  `lifeform_id_fk` INT(11) NOT NULL,
+  `state_id_fk` INT(11) NOT NULL,
+  PRIMARY KEY (`lifeform_id_fk`, `state_id_fk`),
+  INDEX `fk_lifeform_states_states1_idx` (`state_id_fk` ASC) ,
   CONSTRAINT `fk_lifeform_states_lifeforms1`
     FOREIGN KEY (`lifeform_id_fk`)
     REFERENCES `cyberlife`.`lifeforms` (`lifeform_id`)
     ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_lifeform_states_states1`
+    FOREIGN KEY (`state_id_fk`)
+    REFERENCES `cyberlife`.`states` (`state_id`)
+    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
--- Table `cyberlife`.`word_phenomens`
+-- Table `cyberlife`.`phenomens`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cyberlife`.`word_phenomens` (
+CREATE TABLE IF NOT EXISTS `cyberlife`.`phenomens` (
+  `phenomen_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `effect` DOUBLE NOT NULL,
+  `description` VARCHAR(45) NULL DEFAULT NULL,
+  `state_id_fk` INT(11) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`phenomen_id`, `state_id_fk`),
+  UNIQUE INDEX `phenomen_id_UNIQUE` (`phenomen_id` ASC) ,
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC) ,
+  INDEX `fk_phenomens_states1_idx` (`state_id_fk` ASC) ,
+  CONSTRAINT `fk_phenomens_states1`
+    FOREIGN KEY (`state_id_fk`)
+    REFERENCES `cyberlife`.`states` (`state_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `cyberlife`.`world_phenomens`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cyberlife`.`world_phenomens` (
   `area` MULTIPOINT NOT NULL,
-  `gradient_factor` DOUBLE NULL,
+  `gradient_factor` DOUBLE NULL DEFAULT NULL,
   `power_factor` DOUBLE NOT NULL,
-  `phenomen_id_fk` INT NOT NULL,
-  `state_id_fk` INT NOT NULL,
-  `word_id_fk` INT NOT NULL,
-  `status` BIT NOT NULL,
-  `period` INT NULL,
-  PRIMARY KEY (`phenomen_id_fk`, `state_id_fk`, `word_id_fk`),
-  INDEX `fk_word_phenomens_words1_idx` (`word_id_fk` ASC) ,
-  CONSTRAINT `fk_word_phenomens_phenomens1`
-    FOREIGN KEY (`phenomen_id_fk` , `state_id_fk`)
-    REFERENCES `cyberlife`.`phenomens` (`phenomen_id` , `state_id_fk`)
+  `status` BIT(1) NOT NULL,
+  `period` INT(11) NULL DEFAULT NULL,
+  `instance_id` INT NOT NULL AUTO_INCREMENT,
+  `world_id_fk` INT(11) NOT NULL,
+  `phenomen_id_fk` INT(11) NOT NULL,
+  PRIMARY KEY (`instance_id`, `world_id_fk`, `phenomen_id_fk`),
+  UNIQUE INDEX `instance_id_UNIQUE` (`instance_id` ASC) ,
+  INDEX `fk_world_phenomens_worlds1_idx` (`world_id_fk` ASC) ,
+  INDEX `fk_world_phenomens_phenomens1_idx` (`phenomen_id_fk` ASC) ,
+  CONSTRAINT `fk_world_phenomens_worlds1`
+    FOREIGN KEY (`world_id_fk`)
+    REFERENCES `cyberlife`.`worlds` (`world_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_word_phenomens_words1`
-    FOREIGN KEY (`word_id_fk`)
-    REFERENCES `cyberlife`.`words` (`word_id`)
+  CONSTRAINT `fk_world_phenomens_phenomens1`
+    FOREIGN KEY (`phenomen_id_fk`)
+    REFERENCES `cyberlife`.`phenomens` (`phenomen_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
